29eb49f992c342ea7e7c0635371a3c419fa2abfb
salimbeni1 <etienne.salimbeni@epfl.ch>
2022-03-01 21:48:15 +0100

    template 1

--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -1,15 +1,34 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <>
+    <div style={{margin:"0px 20px"}}>
 
-      <h1>ETIENNE SALIMBENI</h1>
+
+      <div className={styles.frontHeader}>
+
+        <div className={styles.title}>
+          <h1 className={styles.title1}> FULL STACK <br/> DEVELOPER</h1>
+          <h1 className={styles.title2}> ETIENNE SALIMBENI</h1>
+        </div>
+        
+      </div>
+
+      <div className={styles.directoriesHeader}>
+
+        <div className={styles.option}>Projects</div>
+        <div className={styles.option}>Skills Set</div>
+        <div className={styles.option}>About Me</div>
+
+      </div>
+     
+
+
 
       
 
-    </>
+    </div>
   )
 }
