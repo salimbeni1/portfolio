4ca637edfbcf92e0c9416b3142ada18b66a4f1b1
salimbeni1 <etienne.salimbeni@epfl.ch>
2023-11-15 13:40:18 +0100

    reafactor all sections

--- web-app/components/academia/index.js
+++ web-app/components/academia/index.js
@@ -5,30 +5,44 @@ export default class Academia extends Component {
   render() {
     return (
       <>
-      <div>
-
+      <div className={styles.main}>
         <h1>Academic Track</h1>
 
-        <div>
-          Cybersecuty EPFL - ETHZ
-          computer science : EPFL 
-          bioengennering : EPFL
-        </div>
-
-        <div>
-          <h3>Publication</h3>
-          <div>
-            Your email adress holds the key, 
+        <div className={styles.main_1}>
+          <div className={styles.uni}>
+            <div>Cybersecuty</div> <div>EPFL - ETHZ</div>
+            <div>computer science</div> <div>EPFL</div>  
+            <div>bioengennering</div> <div>EPFL</div>
           </div>
+          <div className={styles.publications}>
+            <h3>Publication</h3>
+            <div>
+              Your email adress holds the key, 
+            </div>
+          </div>
+        </div>
+        
+        <div className={styles.uni_projects}>
 
           <h3>Selected Academic Projects</h3>
           <div>
-            Thin film irredecence 
+              Thin film irredecence 
           </div>
+          <div>
+              Physic Sim with NCA 
+          </div>
+          <div>
+              HPC
+          </div>
+          <div>
+              MRViz
+          </div>
+          <div>
+              Log Return
+          </div>
+
         </div>
         
-        
-
       </div>
       </>)
     }
--- web-app/components/projects/index.js
+++ web-app/components/projects/index.js
@@ -4,30 +4,69 @@ import styles from './projects.module.scss'
 export default class Projects extends Component {
   render() {
     return (
-      <>
+      <div className={styles.main}>
 
       <h1>Xplore</h1>
+      <div className={styles.xplore}>
+
+        <div className={styles.im1}>
+          <img src='Xplore/xplore_rover.jpg' height={"100%"}/>
+        </div>
+
+        <div className={styles.text}>
+
+          <h2>Navigation Team Lead</h2>
+          <p>Xplore is an EPFL student association that link academia, industry and governmental agencies around Space Rover Technologies. </p>
+          <p>I had the chance to lead the Navigation team of Xplore. Our focus was on the development of Perception, Planning and Localization of a rover in a full autonomous navigation framework  </p>
+
+        </div>
+        <div className={styles.im2}>
+          <div className={styles.im2_1}>
+            <img src='Xplore/xplore_team.jpg' height={"100%"}/>
+          </div>
+          <div className={styles.im2_2}>
+            <div className={styles.im2_2_1}>
+              <img src='Xplore/rviz_xplore.png' height={"100%"}/>
+            </div>
+            <div className={styles.im2_2_2}>
+              <img src='Xplore/xplore_rover_2.jpg' height={"100%"}/>
+            </div>
+          </div>
+        </div>
+
+      </div>
       
       <h1>Personal Projects</h1>
       
-      <div>
-        SVG-Animator
-      </div>
+      <div className={styles.personal}>
+        <div className={styles.svg_animator}>
+          SVG-Animator
+        </div>
 
-      <div>
-        Konvertor
-      </div>
+        <div>
+          Konvertor
+        </div>
 
-      <div>
-        Koalabo
-      </div>
+        <div> </div>
 
-      <div>
-        Volumetric NCA
-      </div>
+        <div>
+          Koalabo
+        </div>
 
-      <br/><br/>
+        <div>
+          Volumetric NCA
+        </div>
+
+        <div>
+          
+        </div>
+
+        <div>
+          UV Photography
+        </div>
+      </div>
       
-      </>)
+      <br/><br/>
+      </div>)
     }
 }
