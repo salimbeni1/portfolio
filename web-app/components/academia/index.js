ef07b74fc2f8e54867cab4ad0f07bd660cf56c13
salimbeni1 <etienne.salimbeni@epfl.ch>
2023-11-17 16:45:20 +0100

    industry description

--- web-app/components/academia/index.js
+++ web-app/components/academia/index.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component , useState } from 'react'
 import styles from './academia.module.scss'
 
-export default class Academia extends Component {
-  render() {
+export default function Academia() {
+
+    const [project, setProject] = useState("")
+
     return (
       <>
       <div className={styles.main}>
@@ -10,40 +12,65 @@ export default class Academia extends Component {
 
         <div className={styles.main_1}>
           <div className={styles.uni}>
-            <div>Cybersecuty</div> <div>EPFL - ETHZ</div>
-            <div>computer science</div> <div>EPFL</div>  
-            <div>bioengennering</div> <div>EPFL</div>
+            <div>Cybersecuty</div> <div>EPFL - ETHZ</div> <div>2020 - 2023</div>
+            <div>Computer Science</div> <div>EPFL</div>  <div>2018 - 2020</div>
+            <div>Bioengennering</div> <div>EPFL</div> <div>2016 - 2018</div>
           </div>
           <div className={styles.publications}>
             <h3>Publication</h3>
             <div>
-              Your email adress holds the key, 
+              <div className={styles.title}> Your Email Address Holds the Key: Understanding the Connection Between Email and Password Security with Deep Learning </div>
+              <div className={styles.authors}>
+                <div> <b>Etienne Salimbeni *</b></div>
+                <div>, Nina Mainusch *</div>
+                <div>, Dario Pasquini</div>
+              </div>
+              <div> Deep Learning and Security Workshop ( IEEE S&P ) </div>
+            
+            
             </div>
           </div>
         </div>
         
+        <h3>Selected Academic Projects</h3>
         <div className={styles.uni_projects}>
 
-          <h3>Selected Academic Projects</h3>
+          
           <div>
-              Thin film irredecence 
+            <div className={styles.project_names}>
+              <div onClick={e => {setProject("Thin Film Irredecence")}}>
+                  Thin Film Irredecence 
+              </div>
+              <div onClick={e => {setProject("Physic Sim with NCA")}}>
+                  Physic Sim with NCA 
+              </div>
+              <div onClick={e => {setProject(p => p ="HPC")}}>
+                  HPC
+              </div>
+              <div onClick={e => {setProject("MRViz")}}>
+                  MRViz
+              </div>
+              <div onClick={e => {setProject("Log Return")}}>
+                  Log Return
+              </div>
+            </div>
           </div>
-          <div>
-              Physic Sim with NCA 
+          
+
+          <div className={styles.project_CONTENT}>
+
+            {project == ""? "< <- <-- <--- Select a Project":<></>}
+            {project == "Thin Film Irredecence"? "Thin Film Irredecence ":<></>}
+            {project == "Physic Sim with NCA"? "Physic Sim with NCA ":<></>}
+            {project == "HPC"? "HPC ":<></>}
+            {project == "MRViz"? "MRViz ":<></>}
+            {project == "Log Return"? "Log Return ":<></>}
+
           </div>
-          <div>
-              HPC
-          </div>
-          <div>
-              MRViz
-          </div>
-          <div>
-              Log Return
-          </div>
+          
 
         </div>
         
       </div>
       </>)
-    }
 }
--- web-app/components/industry/index.js
+++ web-app/components/industry/index.js
@@ -10,14 +10,14 @@ export default class Industry extends Component {
         <h1>Work Experience</h1>
         <div className={styles.Oracle}>
             <div className={styles.Oracle_1}>
-                <div>Oracle</div>
+                <div>Oracle Labs</div>
                 <h3>Research Assistant</h3>
+                <div>Oracle Cloud and AI security Group</div>
             </div>
 
             <div className={styles.Oracle_2}>
-
-                
-
+                ML based anomaly detection of database transactions.
+               Cross-Compilation of python modules with C++ and java bindings from Linux x86 to MacOs arm64
             </div>
 
             <div className={styles.Oracle_3}>
@@ -29,10 +29,14 @@ export default class Industry extends Component {
 
         <div className={styles.Armasuisse}>
             <div className={styles.Armasuisse_1}>
-                <div>Armasuisse</div>
+                <div>Armasuisse CYD</div>
                 <h3>Software engeneer intern</h3>
+                <div>Cyber Defence Campus</div>
             </div>
-            <div className={styles.Armasuisse_2}></div>
+            <div className={styles.Armasuisse_2}>
+                Optimize a large scale network flow anomaly detection pipeline to replace Splunk.
+                Deployment of deception systems within the network DMZ.
+            </div>
             <div className={styles.Armasuisse_3}>
 
             <img src="armasuisse.png" alt="Oracle Logo" width="200" height="auto"/>
@@ -43,8 +47,13 @@ export default class Industry extends Component {
             <div className={styles.Niantic_1}>
                 <div>Niantic Labs</div>
                 <h3>Security engeneer intern</h3>
+                <div>Niantic Infrastructure Security Group</div>
             </div>
-            <div className={styles.Niantic_2}></div>
+            <div className={styles.Niantic_2}> 
+                Automate Google Cloud assets and threats discovery.
+                Run security code review on custom authorization
+                and authentication services: OAuth2, OpenID
+            </div>
             <div className={styles.Niantic_3}>
                 <img src="Niantic.svg" alt="Oracle Logo" width="200" height="auto"/>
             </div>
@@ -56,7 +65,9 @@ export default class Industry extends Component {
                 <div>EPFL</div>
                 <h3>Teaching assistant</h3>
             </div>
-            <div className={styles.EPFL_2}></div>
+            <div className={styles.EPFL_2}>
+            Supervising group projects, Q&A sessions, project and exam corrections for different lectures.
+            </div>
             <div className={styles.EPFL_3}>
                 <img src="EPFL.png" alt="Oracle Logo" width="100" height="auto"/>
             </div>
